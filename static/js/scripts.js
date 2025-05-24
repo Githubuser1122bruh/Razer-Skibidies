b86f2b2769b4a07ac30740a773106a54e7749754
Samhithpola2025 <91000525+Githubuser1122bruh@users.noreply.github.com>
2025-05-23 22:00:32 -0700

    fixed base url

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -14,7 +14,7 @@ let audioCtx = null;
 let animationId = null;
 let isRecording = false;
 
-const BASE_URL = "http://127.0.0.1:5001";
+const BASE_URL = "";
 
 // --- Recording Start ---
 recordButton?.addEventListener("click", async () => {
